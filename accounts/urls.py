9ff10c236f32c4f0aff8e2fffd7a8d6a7ceb6116
CDinuwan <chanukadinuwan35@gmail.com>
2020-05-15 22:02:28 +0530

    Updated user registration form to data save in db

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('register',views.register,name='index')
+    path('register',views.register,name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def register(request):
-    return render(request,'register.html')
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        username=request.POST['username']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        email=request.POST['email']
+
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                print('Username taken')
+            elif email==email:
+                print('email taken')
+            else:
+                user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                user.save();
+                print('user created')
+                return redirect('/')
+        else:
+            print('user pw not match')
+            return redirect('/')
+    else:
+        return render(request,'register.html')
